6cf253d07336dde85eef6acc5634cde2daa6f6e7
Joaquin <joaquin.garcia.nieto@accenture.com>
2024-07-26 17:09:13 +0200

    feat: add theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import MainNav from '../components/MainNav';
 import './globals.css';
+import { ThemeProvider } from '../components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,12 +19,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="flex flex-col items-center border-b mb-5 py-3">
-          <div className="max-w-6x1 w-full">
-            <MainNav />
-          </div>
-        </nav>
-        <main className="flex flex-col items-center">{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className="flex flex-col items-center border-b mb-5 py-3">
+            <div className="max-w-6x1 w-full">
+              <MainNav />
+            </div>
+          </nav>
+          <main className="flex flex-col items-center">{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
